f5d4127802e7b2e8acb4c282c1034e52766a8197
Khoi Nguyen <akhoi90@gmail.com>
2016-04-10 22:11:03 +0700

    [ADD] freco: config using okhttp

--- app/src/main/java/com/xkcn/gallery/XkcnApp.java
+++ app/src/main/java/com/xkcn/gallery/XkcnApp.java
@@ -3,11 +3,16 @@ package com.xkcn.gallery;
 import android.app.Application;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.imagepipeline.backends.okhttp.OkHttpImagePipelineConfigFactory;
+import com.facebook.imagepipeline.core.ImagePipelineConfig;
 import com.khoinguyen.logging.L;
+import com.squareup.okhttp.OkHttpClient;
 import com.xkcn.gallery.di.ApplicationComponent;
 import com.xkcn.gallery.di.ApplicationModule;
 import com.xkcn.gallery.di.DaggerApplicationComponent;
 import com.crashlytics.android.Crashlytics;
+
+import java.util.concurrent.TimeUnit;
 
 import io.fabric.sdk.android.Fabric;
 
@@ -20,10 +25,24 @@ public class XkcnApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Fresco.initialize(this);
+        initFresco();
         initInjector();
         Fabric.with(this, new Crashlytics());
         L.setClassLoggable(BuildConfig.LOGGABLE);
+    }
+
+    private void initFresco() {
+        final int DEFAULT_READ_TIMEOUT_MILLIS = 20 * 1000; // 20s
+        final int DEFAULT_WRITE_TIMEOUT_MILLIS = 20 * 1000; // 20s
+        final int DEFAULT_CONNECT_TIMEOUT_MILLIS = 15 * 1000; // 15s
+        OkHttpClient okHttpClient = new OkHttpClient();
+        okHttpClient.setConnectTimeout(DEFAULT_CONNECT_TIMEOUT_MILLIS, TimeUnit.MILLISECONDS);
+        okHttpClient.setReadTimeout(DEFAULT_READ_TIMEOUT_MILLIS, TimeUnit.MILLISECONDS);
+        okHttpClient.setWriteTimeout(DEFAULT_WRITE_TIMEOUT_MILLIS, TimeUnit.MILLISECONDS);
+        ImagePipelineConfig config = OkHttpImagePipelineConfigFactory
+                .newBuilder(this, okHttpClient)
+                .build();
+        Fresco.initialize(this, config);
     }
 
     private void initInjector() {
