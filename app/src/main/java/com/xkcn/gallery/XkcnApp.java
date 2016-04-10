3d0b51d192c2b99397bc1ac165efda5c1483c742
Khoi Nguyen <akhoi90@gmail.com>
2016-04-11 02:50:37 +0700

    [UPDATE] fresco: use repo at a specified commit

--- app/src/main/java/com/xkcn/gallery/XkcnApp.java
+++ app/src/main/java/com/xkcn/gallery/XkcnApp.java
@@ -3,18 +3,16 @@ package com.xkcn.gallery;
 import android.app.Application;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
-import com.facebook.imagepipeline.backends.okhttp.OkHttpImagePipelineConfigFactory;
+import com.facebook.imagepipeline.backends.okhttp3.OkHttpImagePipelineConfigFactory;
 import com.facebook.imagepipeline.core.ImagePipelineConfig;
 import com.khoinguyen.logging.L;
-import com.squareup.okhttp.OkHttpClient;
 import com.xkcn.gallery.di.ApplicationComponent;
 import com.xkcn.gallery.di.ApplicationModule;
 import com.xkcn.gallery.di.DaggerApplicationComponent;
 import com.crashlytics.android.Crashlytics;
 
-import java.util.concurrent.TimeUnit;
-
 import io.fabric.sdk.android.Fabric;
+import okhttp3.OkHttpClient;
 
 /**
  * Created by khoinguyen on 12/25/14.
@@ -32,13 +30,7 @@ public class XkcnApp extends Application {
     }
 
     private void initFresco() {
-        final int DEFAULT_READ_TIMEOUT_MILLIS = 20 * 1000; // 20s
-        final int DEFAULT_WRITE_TIMEOUT_MILLIS = 20 * 1000; // 20s
-        final int DEFAULT_CONNECT_TIMEOUT_MILLIS = 15 * 1000; // 15s
         OkHttpClient okHttpClient = new OkHttpClient();
-        okHttpClient.setConnectTimeout(DEFAULT_CONNECT_TIMEOUT_MILLIS, TimeUnit.MILLISECONDS);
-        okHttpClient.setReadTimeout(DEFAULT_READ_TIMEOUT_MILLIS, TimeUnit.MILLISECONDS);
-        okHttpClient.setWriteTimeout(DEFAULT_WRITE_TIMEOUT_MILLIS, TimeUnit.MILLISECONDS);
         ImagePipelineConfig config = OkHttpImagePipelineConfigFactory
                 .newBuilder(this, okHttpClient)
                 .build();
