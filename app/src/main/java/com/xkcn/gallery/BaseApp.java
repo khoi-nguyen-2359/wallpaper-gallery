ea504e4cc0dab538cd1e126e88ef203c474a17bf
Khoi Nguyen <akhoi90@gmail.com>
2016-07-21 00:33:16 +0700

    UPDATE - crashlytics

--- app/src/main/java/com/xkcn/gallery/BaseApp.java
+++ app/src/main/java/com/xkcn/gallery/BaseApp.java
@@ -3,12 +3,14 @@ package com.xkcn.gallery;
 import android.app.Application;
 import android.util.Log;
 
+import com.crashlytics.android.Crashlytics;
 import com.facebook.common.logging.FLog;
 import com.khoinguyen.photoviewerkit.impl.PhotoViewerKit;
 import com.khoinguyen.util.log.L;
 import com.xkcn.gallery.di.component.ApplicationComponent;
 import com.xkcn.gallery.di.component.DaggerApplicationComponent;
 import com.xkcn.gallery.di.module.ApplicationModule;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by khoinguyen on 12/25/14.
@@ -19,9 +21,10 @@ public class BaseApp extends Application {
   @Override
   public void onCreate() {
     super.onCreate();
+    Fabric.with(this, new Crashlytics());
     initInjector();
     PhotoViewerKit.init(this);
-    L.setClassLoggable(BuildConfig.LOGGABLE);
+//    L.setClassLoggable(BuildConfig.LOGGABLE);
 //    FLog.setMinimumLoggingLevel(Log.VERBOSE);
   }
 
