53a6468b656ced190efea21a2e78862c655a3754
Khoi Nguyen <akhoi90@gmail.com>
2016-07-23 23:24:13 +0700

    UPDATE - analytics: use preset params of firebase

--- app/src/main/java/com/xkcn/gallery/analytics/FirebaseAnalytics.java
+++ app/src/main/java/com/xkcn/gallery/analytics/FirebaseAnalytics.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.os.Bundle;
 
 import com.xkcn.gallery.data.model.PhotoDetails;
+import com.google.firebase.analytics.FirebaseAnalytics.Param;
 
 /**
  * Created by khoinguyen on 7/20/16.
@@ -11,23 +12,17 @@ import com.xkcn.gallery.data.model.PhotoDetails;
 
 public class FirebaseAnalytics implements IAnalytics {
 
-  private static final String EVENT_SHARE_PHOTO = "share_photo";
-  private static final String EVENT_SET_WALLPAPER = "set_wallpaper";
-  private static final String EVENT_DOWNLOAD = "download";
-
-  private static final String PARAM_PHOTO_TITLE = "photo_title";
-  private static final String PARAM_PHOTO_ID = "photo_id";
-  private static final String PARAM_SCREEN_NAME = "screen_name";
-  private static final String PARAM_PHOTO_CAT_NAME = "category_name";
-  private static final String PARAM_PHOTO_INDEX = "photo_index";
+  private static final String EVENT_SHARE_GALLERY_PHOTO = "share_gallery_photo";
+  private static final String EVENT_SET_GALLERY_WALLPAPER = "set_gallery_wallpaper";
+  private static final String EVENT_DOWNLOAD_GALLERY_PHOTO = "download_gallery_photo";
 
   private static final String VAL_SCREEN_NAME_LISTING = "listing_screen";
   private static final String VAL_SCREEN_NAME_GALLERY = "gallery_screen";
 
   private static Bundle addPhotoParams(Bundle params, PhotoDetails photoDetails) {
     if (params != null && photoDetails != null) {
-      params.putLong(PARAM_PHOTO_ID, photoDetails.getIdentifier());
-      params.putString(PARAM_PHOTO_TITLE, photoDetails.getTitle());
+      params.putLong(Param.ITEM_ID, photoDetails.getIdentifier());
+      params.putString(Param.ITEM_NAME, photoDetails.getPermalinkMetaAsText());
     }
 
     return params;
@@ -42,14 +37,14 @@ public class FirebaseAnalytics implements IAnalytics {
   @Override
   public void trackListingEndScroll(String categoryName, int lastPhotoIndex) {
     Bundle params = new Bundle();
-    params.putString(PARAM_PHOTO_CAT_NAME, categoryName);
-    params.putInt(PARAM_PHOTO_INDEX, lastPhotoIndex);
+    params.putString(Param.ITEM_CATEGORY, categoryName);
+    params.putInt(Param.ITEM_LOCATION_ID, lastPhotoIndex);
     firebaseAnalytics.logEvent("listing_end_scroll", params);
   }
 
   private void trackScreenView(String screenName) {
     Bundle params = new Bundle();
-    params.putString(PARAM_SCREEN_NAME, screenName);
+    params.putString(Param.ITEM_NAME, screenName);
     firebaseAnalytics.logEvent("screen_view", params);
   }
 
@@ -66,21 +61,18 @@ public class FirebaseAnalytics implements IAnalytics {
   @Override
   public void trackShareGalleryPhoto(PhotoDetails photoDetails) {
     Bundle params = addPhotoParams(new Bundle(), photoDetails);
-    params.putString(PARAM_SCREEN_NAME, VAL_SCREEN_NAME_GALLERY);
-    firebaseAnalytics.logEvent(EVENT_SHARE_PHOTO, params);
+    firebaseAnalytics.logEvent(EVENT_SHARE_GALLERY_PHOTO, params);
   }
 
   @Override
   public void trackSetWallpaperGalleryPhoto(PhotoDetails photoDetails) {
     Bundle params = addPhotoParams(new Bundle(), photoDetails);
-    params.putString(PARAM_SCREEN_NAME, VAL_SCREEN_NAME_GALLERY);
-    firebaseAnalytics.logEvent(EVENT_SET_WALLPAPER, params);
+    firebaseAnalytics.logEvent(EVENT_SET_GALLERY_WALLPAPER, params);
   }
 
   @Override
   public void trackDownloadGalleryPhoto(PhotoDetails photoDetails) {
     Bundle params = addPhotoParams(new Bundle(), photoDetails);
-    params.putString(PARAM_SCREEN_NAME, VAL_SCREEN_NAME_GALLERY);
-    firebaseAnalytics.logEvent(EVENT_DOWNLOAD, params);
+    firebaseAnalytics.logEvent(EVENT_DOWNLOAD_GALLERY_PHOTO, params);
   }
 }
